939fbf261f3003e3ab4f4dda4584354e9ff7d83a
David Spackman <dasp@stibosystems.com.au>
2022-10-04 14:24:34 +1100

    Move code into functions; remove some unrequired code that is superseded by using the screen class

--- WebUIAnalysis.py
+++ WebUIAnalysis.py
@@ -3,13 +3,12 @@ import json
 import operator
 from random import random
 
-
 referencedScreen = []
 allScreen = []
 unusedScreen = []
-
-items = dict()
 screenItems = dict()
+
+namespaces = {'ns':'http://stibosystems.com/step/portal-config'}
 
 class Screen:
     def __init__(self, id, type):
@@ -18,14 +17,6 @@ class Screen:
         self.count = 0
     def __str__(self):
         return f"ID: {self.id}, Type: {self.type}, Count: {self.count}"
-
-
-def count_this(screenID):
-    global items
-    if screenID not in items.keys():
-        items[screenID] = 0
-    else:
-        items[screenID] += 1
 
 def initialise_class(screenID, ScreenType):
     global screenItems
@@ -45,14 +36,12 @@ def count_this_class(screenID):
         screenObject.count += 1
         screenItems[screenID] = screenObject
 
-def main():
+def printList(list):
+    list.sort()      
+    for object in list:
+       print(object)
 
-    doc = etree.parse('webUI.xml')
-    namespaces = {'ns':'http://stibosystems.com/step/portal-config'}
-
-
-    screens = doc.xpath("//ns:screen[not(@type = 'Main' or @type = 'HomePage' or @type = 'LoginScreen')]",namespaces=namespaces)
-
+def determineTotalScreens(screens):
     #Total Screens
     for screen in screens:
         screenID = screen.xpath('@id')[0]
@@ -60,8 +49,10 @@ def main():
         #print("ScreenID",screenID)
         #print("ScreenType",ScreenType)
         allScreen.append(screenID)
-        count_this(screenID)
         initialise_class(screenID, ScreenType)
+
+def determineReferencedScreens(doc, screens):
+    global namespaces
 
     #Referenced Screens - Check 1
     for screen in screens:
@@ -69,7 +60,6 @@ def main():
         btags = screen.xpath("ns:component[@id='Main'][@type='TabControl']/ns:parameter-list/ns:component/ns:parameter[@id='DetailsScreen']/@value",namespaces=namespaces)
         for b in btags:
             # print(" - ",b)
-            count_this(b)
             count_this_class(b)
             if not b in referencedScreen:
                 #print("Item is in array already.")
@@ -78,7 +68,6 @@ def main():
     #Referenced Screens - Check 2
     forwardingSwitchScreenScreen = doc.xpath("//ns:screen[@type='ForwardingSwitchScreen']/ns:parameter-list[@id='Mappings']/ns:component/ns:parameter[@id='Screen']/@value",namespaces=namespaces)
     for screen in forwardingSwitchScreenScreen:
-        count_this(screen)
         count_this_class(screen)
         if not screen in referencedScreen:
             #print("Item is in array already.")
@@ -88,7 +77,6 @@ def main():
     mainScreen = doc.xpath("//ns:screen[@id='main']/ns:parameter-list[@id='Mappings']/ns:component/ns:parameter[@id='Screen']/@value",namespaces=namespaces)
     for screen in mainScreen:
         #print("mainScreen", screen)
-        count_this(screen)
         count_this_class(screen)
         if not screen in referencedScreen:
             #print("Item is in array already.")
@@ -98,46 +86,37 @@ def main():
     homepageScreen = doc.xpath("//ns:screen[@id='homepage']/ns:parameter-list/ns:component/ns:parameter-list/ns:component/ns:parameter[@id='ResultScreen']/@value",namespaces=namespaces)
     for screen in homepageScreen:
         #print("homepageScreen", screen)
-        count_this(screen)
         count_this_class(screen)
         if not screen in referencedScreen:
             #print("Item is in array already.")
             referencedScreen.append(screen)
-    
-    #Unused Screens
+
+def main():
+
+    doc = etree.parse('webUI.xml')
+    screens = doc.xpath("//ns:screen[not(@type = 'Main' or @type = 'HomePage' or @type = 'LoginScreen')]",namespaces=namespaces)
+
+    determineTotalScreens(screens)
+    determineReferencedScreens(doc, screens)
+
+    #Create List of Unused Screens
     for screen in allScreen:
         if screen not in referencedScreen:
             unusedScreen.append(screen) 
 
-    #allScreen.sort()      
-    #for allScreenitem in allScreen:
-    #    print(allScreenitem)
-
-    #referencedScreen.sort()      
-    #for referencedScreenitem in referencedScreen:
-    #    print(referencedScreenitem)
-
-    #unusedScreen.sort() 
-    #for unusedScreenitem in unusedScreen:
-    #    print(unusedScreenitem)
-
-    
-    # sortedDict = {k: v for k, v in sorted(items.items(), key=lambda item: item[1])}
-    # print(json.dumps(sortedDict, indent='\t'))
-
-    # print()
-    # print("Total Screen Count:",len(allScreen))
-    # print("Referenced Screen Count:",len(referencedScreen))
-    # print("(Potential) Unused Screen Count:",len(unusedScreen))
-
-    # for key in screenItems:
-    #     print(screenItems[key])
+    # printList(allScreen)
+    # printList(referencedScreen)
+    # printList(unusedScreen)
 
     print()
-    print()
-
-
+    print("Screen Metadata")
     for screenItem in (sorted(screenItems.values(), key=operator.attrgetter('count'))):
         print(screenItem)
+
+    print()
+    print("Screen Summary")
+    print("Total Screen Count:", len(allScreen))
+    print("Referenced Screen Count:", len(referencedScreen))
+    print("(Potential) Unused Screen Count:", len(unusedScreen))
 
 if __name__ == '__main__': main()
