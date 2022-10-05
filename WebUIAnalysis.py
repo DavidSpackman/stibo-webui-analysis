bcf6780d8eaf4e0f9c86dd0aaa765f2dc3ea1983
David Spackman <dasp@stibosystems.com.au>
2022-10-06 10:07:34 +1100

    Add capability to breakdown when the screen is used. Still not exposed.

--- WebUIAnalysis.py
+++ WebUIAnalysis.py
@@ -13,9 +13,15 @@ class Screen:
     def __init__(self, id, type):
         self.id = id
         self.type = type
-        self.count = 0
+        self.totalCount = 0
+        self.subScreenTabPageCount = 0
+        self.mappingCount = 0
+        self.forwardingScreenCount = 0
+        self.resultScreenCount = 0
+
     def __str__(self):
-        return f"ID: {self.id}, Type: {self.type}, Count: {self.count}"
+        #return f"ID: {self.id}, Type: {self.type}, Total: {self.totalCount}, subScreenTabPageCount: {self.subScreenTabPageCount}, mappingCount: {self.mappingCount}, forwardingScreenCount: {self.forwardingScreenCount}, resultScreenCount: {self.resultScreenCount}"
+        return f"ID: {self.id}, Type: {self.type}, Total: {self.totalCount}"
 
 def initialise_class(screenID, ScreenType):
     global screenItems
@@ -26,14 +32,25 @@ def initialise_class(screenID, ScreenType):
     else:
         errors.append("Function: initialise_class, ScreenID: " + screenID)
 
-def count_this_class(screenID):
+def count_this_class(screenID, screenType):
     global screenItems
     global errors
     if screenID not in screenItems.keys():
         errors.append("Function: count_this_class, ScreenID: " + screenID)
     else:
         screenObject = screenItems[screenID]
-        screenObject.count += 1
+        screenObject.totalCount += 1
+
+        match screenType:
+            case "subScreenTabPage":
+                screenObject.subScreenTabPageCount += 1
+            case "mapping":
+                screenObject.mappingCount += 1
+            case "forwardingScreen":
+                screenObject.forwardingScreenCount += 1
+            case "resultScreen":
+                screenObject.resultScreenCount += 1
+
         screenItems[screenID] = screenObject
 
 def printList(list):
@@ -54,39 +71,39 @@ def determineTotalScreens(screens):
 def determineReferencedScreens(doc, screens):
     global namespaces
 
-    #Referenced Screens - Check 1
+    #Referenced Screens - Check 1: Subscreens
     for screen in screens:
         #https://www.online-toolz.com/tools/xpath-tester-online.php
-        btags = screen.xpath("ns:component[@id='Main'][@type='TabControl']/ns:parameter-list/ns:component/ns:parameter[@id='DetailsScreen']/@value",namespaces=namespaces)
+        btags = screen.xpath("ns:component/ns:parameter-list/ns:component[@type='SubScreenTabPage']/ns:parameter[@id='DetailsScreen']/@value",namespaces=namespaces)
         for b in btags:
             # print(" - ",b)
-            count_this_class(b)
+            count_this_class(b, "subScreenTabPage")
             if not b in referencedScreen:
                 #print("Item is in array already.")
                 referencedScreen.append(b)
 
-    #Referenced Screens - Check 2
+    #Referenced Screens - Check 2: ForwardingSwitchScreens
     forwardingSwitchScreenScreen = doc.xpath("//ns:screen[@type='ForwardingSwitchScreen']/ns:parameter-list[@id='Mappings']/ns:component/ns:parameter[@id='Screen']/@value",namespaces=namespaces)
     for screen in forwardingSwitchScreenScreen:
-        count_this_class(screen)
+        count_this_class(screen, "forwardingScreen")
         if not screen in referencedScreen:
             #print("Item is in array already.")
             referencedScreen.append(screen)
     
-    #Referenced Screens - Check 3
+    #Referenced Screens - Check 3: Mappings (under Main)
     mainScreen = doc.xpath("//ns:screen[@id='main']/ns:parameter-list[@id='Mappings']/ns:component/ns:parameter[@id='Screen']/@value",namespaces=namespaces)
     for screen in mainScreen:
         #print("mainScreen", screen)
-        count_this_class(screen)
+        count_this_class(screen, "mapping")
         if not screen in referencedScreen:
             #print("Item is in array already.")
             referencedScreen.append(screen)
     
-    #Referenced Screens - Check 4
+    #Referenced Screens - Check 4: Results screens
     homepageScreen = doc.xpath("//ns:screen[@id='homepage']/ns:parameter-list/ns:component/ns:parameter-list/ns:component/ns:parameter[@id='ResultScreen']/@value",namespaces=namespaces)
     for screen in homepageScreen:
         #print("homepageScreen", screen)
-        count_this_class(screen)
+        count_this_class(screen, "resultScreen")
         if not screen in referencedScreen:
             #print("Item is in array already.")
             referencedScreen.append(screen)
@@ -110,7 +127,8 @@ def main():
 
     print()
     print("--- Screen Metadata ---")
-    for screenItem in (sorted(screenItems.values(), key=operator.attrgetter('count'))):
+    sortedScreenItems = sorted(screenItems.values(), key=operator.attrgetter('totalCount'))
+    for screenItem in sortedScreenItems:
         print(screenItem)
 
     print()
